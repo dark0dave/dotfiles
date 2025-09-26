b388f4500e61113c4a010e4fe337eecf2fcad325
dark0dave <dark0dave@mykolab.com>
2025-09-26 20:58:11 +0100

    feat(fish): Updates for fish
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,4 +4,12 @@ if status is-interactive
     fish_add_path -g $HOME/.local/bin
 end
 
+if uwsm check may-start
+    exec uwsm start hyprland-uwsm.desktop
+end
+
+alias gf='git fetch -vfpPt --all --update-shallow -j 5'
+
+direnv hook fish | source
+
 ~/.local/bin/mise activate fish | source
